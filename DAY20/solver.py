ad5422883ec9010dfa6999c46583382593fc6952
Arunoday Sarkar <asarka696@cable.comcast.com>
2021-01-14 21:38:11 -0500

    add animation for backtracking and some code cleanup

--- DAY20/solver.py
+++ DAY20/solver.py
@@ -7,8 +7,6 @@ from enum import Enum
 from functools import lru_cache
 from math import sqrt
 
-__DEBUG__ = False
-
 
 class CornerType(Enum):
     TOP = "top"
@@ -31,6 +29,10 @@ class Colors:
     RED = "\031[47m"
     LGRAY = "\033[37m"
     ORANGE = "\033[33m"
+
+
+def ColoredText(color: Colors, text: str):
+    return f"{color}{text}{Colors.ENDC}"
 
 
 def print_grid(grid, color=None):
@@ -130,6 +132,7 @@ class Board:
         self.C = -1
         self.tiles = []
         self.tiles_count = 0
+        self.filled_grid = None
         self.read_layout(file_name)
         self.init_board()
 
@@ -190,15 +193,128 @@ class Board:
             return True
         return False
 
-    def search_monster(self, pattern):
-        pass
+    def search_monster(self, pattern: list):
+        raise NotImplementedError
+
+    def remove_rows_and_colos(self, pos: list, data: list):
+        r_start, r_end, c_start, c_end, block_size = (
+            pos[0],
+            pos[1],
+            pos[2],
+            pos[3],
+            10,
+        )
+        trimmed_size = block_size - 2
+        grid = [["."] * trimmed_size for _ in range(trimmed_size)]
+        i = 0
+        for r in range(r_start, r_end):
+            j = 0
+            for c in range(c_start, c_end):
+                # first row
+                if i == 0 and j in range(0, block_size):
+                    continue
+                elif i == block_size - 1 and j in range(0, block_size):
+                    continue
+                elif j == 0 and i in range(0, block_size):
+                    continue
+                elif j == block_size - 1 and i in range(0, block_size):
+                    continue
+                else:
+                    grid[r][c] = data[i][j]
+
+                j += 1
+            i += 1
 
     # probably needed for the second part
+    def print_grid(self, grid):
+        if not grid:
+            raise Exception("Not a valid grid row")
+        for row in grid:
+            if isinstance(row, list):
+                print(f"{''.join(row)}")
+            else:
+                print(f"{row}")
+
+    def fill_grid_by_pos(self, grid, data, pos):
+        r_start, r_end, c_start, c_end, content_length = (
+            pos[0],
+            pos[1],
+            pos[2],
+            pos[3],
+            10,
+        )
+        i = 0
+        for r in range(r_start, r_end):
+            j = 0
+            for c in range(c_start, c_end):
+                # first row
+                if i == 0 and j in range(0, content_length):
+                    grid[r][c] = ColoredText(Colors.ORANGE, data[i][j])
+                elif i == content_length - 1 and j in range(0, content_length):
+                    grid[r][c] = ColoredText(Colors.ORANGE, data[i][j])
+                elif j == 0 and i in range(0, content_length):
+                    grid[r][c] = ColoredText(Colors.ORANGE, data[i][j])
+                elif j == content_length - 1 and i in range(0, content_length):
+                    grid[r][c] = ColoredText(Colors.ORANGE, data[i][j])
+                else:
+                    grid[r][c] = ColoredText(Colors.LGRAY, data[i][j])
+
+                j += 1
+            i += 1
+
+    def default_data(self):
+        ret = []
+        block_size = 10
+        for r in range(block_size):
+            cols = []
+            for c in range(block_size):
+                cols.append(".")
+            ret.append("".join(cols))
+        return ret
+
+    def fill_grid(self, visited, should_print=False):
+        block_size = 10
+        grid = [["."] * self.C * block_size for _ in range(self.R * block_size)]
+        grid_size = block_size * self.R  # its a square grid
+        start_r = 0
+        for row in range(self.R):
+            start_c = 0
+            for col in range(self.C):
+                tile = visited[row][col]
+                raw_data = (
+                    tile.raw_data if tile is not None else self.default_data()
+                )
+                end_r, end_c = start_r + block_size, start_c + block_size
+                self.fill_grid_by_pos(
+                    grid, raw_data, [start_r, end_r, start_c, end_c]
+                )
+                start_c += block_size
+            start_r += block_size
+
+        # memory inefficient
+        self.filled_grid = grid
+
+        if should_print:
+            self.print_grid(grid)
+
+    def debug_print(self, data, r, c):
+        if not __DEBUG__:
+            pass
+        self.fill_grid(self.visited)
+        block_size = 10
+        start_r, start_c = r * block_size, c * block_size
+        end_r, end_c = start_r + block_size, start_c + block_size
+        self.fill_grid_by_pos(
+            self.filled_grid, data, [start_r, end_r, start_c, end_c]
+        )
+        print("\033[2J\033[1;1H")
+        self.print_grid(self.filled_grid)
+        time.sleep(0.1)
 
     def solve_r(self, r, c, checked=set(), cnt=0):
         R, C = self.R, self.C
         if r == R:
-            # print(f"total recursive calls {cnt}")
+            print(f"total recursive calls {cnt}")
             print("simulation complete")
             print(
                 self.visited[0][0].tile_id,
@@ -213,10 +329,13 @@ class Board:
                 * int(self.visited[R - 1][C - 1].tile_id)
             )
             print(f"answer {answer}")
+            self.fill_grid(self.visited, True)
             exit(0)
         for tile in self.tiles:
             tile_id = tile.tile_id
             if not tile_id in checked:
+                tile_data = tile.raw_data
+                self.debug_print(tile_data, r, c)
                 if r > 0 and not tile.can_place_bottom(
                     r, c, self.R, self.C, self.visited
                 ):
@@ -245,75 +364,14 @@ class Board:
             print(ex.__traceback__())
 
 
-def fill_grid_by_pos(grid, data, pos):
-    r_start, r_end, c_start, c_end, content_length = (
-        pos[0],
-        pos[1],
-        pos[2],
-        pos[3],
-        10,
-    )
-    i = 0
-    for r in range(r_start, r_end):
-        j = 0
-        for c in range(c_start, c_end):
-            if "#" in data[i][j] or "." in data[i][j]:
-                # colorize the corner edges
-                # up.append(data[0][idx])
-                # down.append(data[edge_length - 1][idx])
-                # right.append(data[idx][edge_length - 1])
-                # left.append(data[idx][0])
-                colored_data = ""
-                if r == 0:
-                    grid[r][c] = f"{Colors.ORANGE}{data[i][j]}{Colors.ENDC}"
-                elif r == 9:
-                    grid[r][c] = f"{Colors.ORANGE}{data[i][j]}{Colors.ENDC}"
-                elif c == 0:
-                    grid[r][c] = f"{Colors.ORANGE}{data[i][j]}{Colors.ENDC}"
-                elif c == 9:
-                    grid[r][c] = f"{Colors.ORANGE}{data[i][j]}{Colors.ENDC}"
-                else:
-                    grid[r][c] = f"{Colors.OKGREEN}{data[i][j]}{Colors.ENDC}"
-            else:
-                grid[r][c] = (
-                    f"{Colors.OKBLUE}#{Colors.ENDC}"
-                    if data[i][j] == "1"
-                    else f"{Colors.OKBLUE}.{Colors.ENDC}"
-                )
-            j += 1
-        i += 1
-
-
-def fill_grid_generic(visited, tiles: dict):
-    grid_size = 30
-    blue_zeros = f"{Colors.OKBLUE}0{Colors.ENDC}"
-    grid = [["."] * grid_size for _ in range(grid_size)]
-    l = 10
-    i, j = 0, 0
-    start_r = 0
-    for r in range(3):
-        start_c = 0
-        for c in range(3):
-            j += 10
-            tile_id = visited[r][c]
-            if tile_id == -1:
-                continue
-            tile = tiles.get(tile_id)
-            content = tile.grid_lines
-            end_r, end_c = start_r + 10, start_c + 10
-            fill_grid_by_pos(grid, content, [start_r, end_r, start_c, end_c])
-            start_c += 10
-        start_r += 10
-    return grid
-    # print_grid(grid, Colors.OKCYAN)
-
-
+# Global Program flags
 __SOLVE__ = True
+__DEBUG__ = False
 
 
 def read_input():
     try:
-        board = Board("day_20.in")
+        board = Board("day_20_small.in")
         board()
     except FileNotFoundError as file_not_found_error:
         print(f"file not found error {file_not_found_error}")
